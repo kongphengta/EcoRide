ce305e43c77162d57a60ea23418b098350f639db
vixay kongphengta <kong.vixay@gmail.com>
2025-07-03 10:06:20 +0200

    modification files

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,16 @@
 /* assets/app.js */
 import './bootstrap.js'; 
 /*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * Welcome to your app's main JavaScript file, managed by Webpack Encore.
+ * This file, and any CSS it imports, will be compiled and included on the page
+ * via the `encore_entry_script_tags()` and `encore_entry_link_tags()` Twig functions.
  */
 import './styles/app.css';
 import './styles/profile.css';
 import $ from 'jquery'; // Import jQuery
 import 'bootstrap'; // Import Bootstrap JavaScript
 global.$ = global.jQuery = $; // Make jQuery available globally
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+console.log('This log comes from assets/app.js - Webpack Encore is running!');
 
 
 
